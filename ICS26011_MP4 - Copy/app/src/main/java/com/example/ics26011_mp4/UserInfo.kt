1e1ee63ffb839910e7c9dda203329606624ab0f7
Ira Rayzel S. Ji <103237256+irarayzelji2002@users.noreply.github.com>
2023-11-02 17:48:23 +0800

    Delete ICS26011_MP4 - Copy directory

--- ICS26011_MP4 - Copy/app/src/main/java/com/example/ics26011_mp4/UserInfo.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ics26011_mp4
-
-data class UserInfo(var username : String, var password : String,
-                    var firstname : String, var lastname : String,
-                    var email : String, var mobilenumber : String,
-                    var birthday : String, var access : String)
